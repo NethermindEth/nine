77ddb17572d276dd10765ba6c302eb94c7a6d500
Denis Kolodin <deniskolodin@gmail.com>
2025-02-21 18:33:51 +0100

    Implement the Particle trait for the LiquidParticle

--- substance/n9-kit/src/liquid_tool.rs
+++ substance/n9-kit/src/liquid_tool.rs
@@ -1,7 +1,32 @@
-use crb::agent::{Agent, AgentSession};
+use anyhow::{anyhow, Result};
+use async_trait::async_trait;
+use crb::agent::{Agent, AgentSession, Context};
+use n9_core::{Tool, ToolInput, SubstanceLinks, Particle};
 
-pub struct LiquidTool;
+pub trait LiquidTool {
+}
 
-impl Agent for LiquidTool {
+pub struct LiquidParticle {
+    substance: SubstanceLinks,
+}
+
+impl Particle for LiquidParticle {
+    fn construct(substance: SubstanceLinks) -> Self {
+        Self {
+            substance,
+        }
+    }
+}
+
+impl Agent for LiquidParticle {
     type Context = AgentSession<Self>;
+}
+
+#[async_trait]
+impl<T> Tool<T> for LiquidParticle
+where T: ToolInput,
+{
+    async fn call_tool(&mut self, input: T, _ctx: &mut Context<Self>) -> Result<T::ToolOutput> {
+        Err(anyhow!("Not implemented"))
+    }
 }

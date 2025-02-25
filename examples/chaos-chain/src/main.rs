be4049dced3c877ae671529d3117f56733711d86
Denis Kolodin <deniskolodin@gmail.com>
2025-02-25 14:24:21 +0100

    Add a diagnostics tool implementation for the system info particle

--- examples/chaos-chain/src/main.rs
+++ examples/chaos-chain/src/main.rs
@@ -3,6 +3,7 @@ use n9_app_tui::TuiApp;
 use n9_control_chat::ChatParticle;
 use n9_core::Substance;
 use n9_model_openai::OpenAIParticle;
+use n9_system_info::SystemInfo;
 use ui9_mesh::Mesh;
 
 #[tokio::main]
@@ -13,6 +14,7 @@ async fn main() -> Result<()> {
     substance.add_particle::<OpenAIParticle>()?;
     substance.add_particle::<ChatParticle>()?;
     substance.add_particle::<TuiApp>()?;
+    substance.add_particle::<SystemInfo>()?;
     substance.join().await?;
     Mesh::deactivate().await?;
     Ok(())
--- particles/system-info/src/particle.rs
+++ particles/system-info/src/particle.rs
@@ -1,16 +1,56 @@
-use crb::agent::{Agent, AgentSession};
-use n9_core::{Particle, SubstanceLinks};
+use crate::tools::ToolsList;
+use anyhow::Result;
+use async_trait::async_trait;
+use crb::agent::{Agent, AgentSession, Context, DoAsync, Next};
+use crb::core::Slot;
+use n9_core::{Particle, SubstanceBond, SubstanceLinks, Tool};
 
 pub struct SystemInfo {
     substance: SubstanceLinks,
+    bond: Slot<SubstanceBond<Self>>,
 }
 
 impl Particle for SystemInfo {
     fn construct(substance: SubstanceLinks) -> Self {
-        Self { substance }
+        Self {
+            substance,
+            bond: Slot::empty(),
+        }
     }
 }
 
 impl Agent for SystemInfo {
     type Context = AgentSession<Self>;
+
+    fn begin(&mut self) -> Next<Self> {
+        Next::do_async(Initialize)
+    }
+}
+
+struct Initialize;
+
+#[async_trait]
+impl DoAsync<Initialize> for SystemInfo {
+    async fn handle(&mut self, _: Initialize, ctx: &mut Context<Self>) -> Result<Next<Self>> {
+        let mut bond = self.substance.bond(&ctx);
+        bond.add_tool::<ToolsList>(self).await?;
+        self.bond.fill(bond)?;
+        Ok(Next::events())
+    }
+}
+
+#[async_trait]
+impl Tool<ToolsList> for SystemInfo {
+    fn name(&self) -> String {
+        "tools_list".into()
+    }
+
+    async fn call_tool(
+        &mut self,
+        input: ToolsList,
+        _ctx: &mut Context<Self>,
+    ) -> Result<Vec<String>> {
+        let tools = vec!["Tool 1".to_string()];
+        Ok(tools)
+    }
 }

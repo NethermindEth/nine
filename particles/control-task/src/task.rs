0eccfb794cc56dcc658e083787cf4bd48870fb7d
Denis Kolodin <deniskolodin@gmail.com>
2025-02-27 18:54:31 +0100

    Add the timer to the chat task agent

--- particles/control-task/src/task.rs
+++ particles/control-task/src/task.rs
@@ -1,12 +1,25 @@
+use crate::tools::TaskInfo;
 use anyhow::Result;
 use async_trait::async_trait;
-use crb::agent::{Agent, AgentSession, Context, Next, DoAsync};
+use crb::agent::{Agent, Context, Next, DoAsync, OnEvent};
+use crb::superagent::{Timer, StreamSession, Timeout};
 
 pub struct ChatTask {
+    task_info: TaskInfo,
+    timer: Timer,
+}
+
+impl ChatTask {
+    pub fn new(task_info: TaskInfo) -> Self {
+        Self {
+            task_info,
+            timer: Timer::new(),
+        }
+    }
 }
 
 impl Agent for ChatTask {
-    type Context = AgentSession<Self>;
+    type Context = StreamSession<Self>;
 
     fn begin(&mut self) -> Next<Self> {
         Next::do_async(Initialize)
@@ -18,6 +31,16 @@ struct Initialize;
 #[async_trait]
 impl DoAsync<Initialize> for ChatTask {
     async fn handle(&mut self, _: Initialize, ctx: &mut Context<Self>) -> Result<Next<Self>> {
+        ctx.consume(self.timer.events()?);
         Ok(Next::events())
+    }
+}
+
+#[async_trait]
+impl OnEvent<Timeout> for ChatTask {
+    async fn handle(&mut self, _: Timeout, _ctx: &mut Context<Self>) -> Result<()> {
+        if self.task_info.repeat {
+        }
+        Ok(())
     }
 }

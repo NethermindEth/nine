1d043a8fd82611704531e16c7ba81ce3a136abe2
Denis Kolodin <deniskolodin@gmail.com>
2025-03-07 15:21:36 +0100

    Add the chats list view

---
+++ particles/app-web/ui/src/widgets/chats_list.rs
@@ -0,0 +1,23 @@
+use crate::render::{single, SubComponent, SubContext, SubWidget};
+use ui9_dui::tracers::event::Event;
+use yew::{html, Html};
+
+pub type ChatsListWidget = SubWidget<ChatsListComponent>;
+
+pub struct ChatsListComponent {}
+
+impl SubComponent for ChatsListComponent {
+    type Projection = single::Flow<Event>;
+    type Message = ();
+
+    fn create() -> Self {
+        Self {}
+    }
+
+    fn render(&self, state: single::State<Event>, _ctx: &SubContext<Self>) -> Option<Html> {
+        let typ = std::any::type_name::<Event>();
+        Some(html! {
+            <div>{ format!("Loaded: {typ}") }</div>
+        })
+    }
+}
--- particles/app-web/ui/src/widgets/mod.rs
+++ particles/app-web/ui/src/widgets/mod.rs
@@ -1,10 +1,12 @@
 mod chat;
+mod chats_list;
 pub mod dashboard;
 mod events;
 mod jobs;
 mod peers;
 
 pub use chat::ChatWidget as Chat;
+pub use chats_list::ChatsListWidget as ChatsList;
 pub use events::EventsWidget as Events;
 pub use jobs::JobsWidget as Jobs;
 pub use peers::PeersWidget as Peers;

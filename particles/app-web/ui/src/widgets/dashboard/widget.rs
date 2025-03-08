4af42f55885897d2944d9a266dca763d4f4e53a2
Denis Kolodin <deniskolodin@gmail.com>
2025-03-08 20:10:13 +0100

    Add sidebars to the dashboard

--- particles/app-web/ui/src/widgets/dashboard/widget.rs
+++ particles/app-web/ui/src/widgets/dashboard/widget.rs
@@ -20,25 +20,40 @@ impl SubComponent for DashboardComponent {
     }
 
     fn render(&self, state: single::State<Dashboard>, _ctx: &SubContext<Self>) -> Option<Html> {
-        let chat_or_peers;
-        if let Some(active_peer) = state.active_peer {
-            let peer = active_peer.to_string();
-            let chat_link = FqnLink::remote(Chat::fqn(), active_peer);
-            let jobs_link = FqnLink::remote(Job::fqn(), active_peer);
-            chat_or_peers = html! {
-                <div>
-                    <div>{ "Chat of the peer: " }{ peer }</div>
-                    <widgets::Chat link={chat_link} />
-                    <widgets::Jobs link={jobs_link} />
-                </div>
-            };
-        } else {
-            let first: FqnLink = Peer::fqn().into();
-            let second: FqnLink = Dashboard::fqn().into();
-            chat_or_peers = html! {
-                <widgets::Peers {first} {second} />
-            };
-        }
+        let app_content = {
+            if let Some(active_peer) = state.active_peer {
+                let peer = active_peer.to_string();
+                let chat_link = FqnLink::remote(Chat::fqn(), active_peer);
+                let jobs_link = FqnLink::remote(Job::fqn(), active_peer);
+                html! {
+                    <div class="app-content">
+                        <div class="app-content-left">
+                        </div>
+                        <div class="app-content-center">
+                            <div>{ "Chat of the peer: " }{ peer }</div>
+                            <widgets::Chat link={chat_link} />
+                            <widgets::Jobs link={jobs_link} />
+                        </div>
+                        <div class="app-content-right">
+                        </div>
+                    </div>
+                }
+            } else {
+                let first: FqnLink = Peer::fqn().into();
+                let second: FqnLink = Dashboard::fqn().into();
+                html! {
+                    <div class="app-content">
+                        <div class="app-content-left">
+                        </div>
+                        <div class="app-content-center">
+                            <widgets::Peers {first} {second} />
+                        </div>
+                        <div class="app-content-right">
+                        </div>
+                    </div>
+                }
+            }
+        };
         html! {
             <div class="app">
                 <div class="app-header">
@@ -51,9 +66,7 @@ impl SubComponent for DashboardComponent {
                     <div class="app-header-title">{ "N9 Dashboard" }</div>
                 </div>
 
-                <div class="app-content">
-                    { chat_or_peers }
-                </div>
+                { app_content }
             </div>
         }
         .into()

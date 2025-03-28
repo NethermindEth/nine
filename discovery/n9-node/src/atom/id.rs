71699ff225ace36939919f669ad423e433dda264
Denis Kolodin <deniskolodin@gmail.com>
2025-03-28 22:46:45 +0100

    Add queries and deltas

--- discovery/n9-node/src/atom/id.rs
+++ discovery/n9-node/src/atom/id.rs
@@ -8,6 +8,7 @@ pub type ElementId = String;
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct AtomId {
     pub peer_id: PeerId,
+    // TODO: Replace with Fqn
     pub path: Vec<ElementId>,
 }
 
--- discovery/n9-node/src/atom/state.rs
+++ discovery/n9-node/src/atom/state.rs
@@ -1,6 +1,69 @@
 // TODO: Copy the flow, but:
-// 1. Event => Delta
-// 2. Action => Query
-// 3. fn divide() # get the state to share (filled by Notification)
 // 4. Add state_instance_id to send deltas directly
 // 5. Provide state_instance_id with a query
+
+use super::encoding::{FlexCodec, PackedAction, PackedEvent, PackedState, ProtocolCodec};
+use anyhow::Result;
+use serde::{de::DeserializeOwned, Serialize};
+
+/// Requirements for a data fraction in a data flow.
+/// `Sync` is needed to access through the `watch` channel of the `Player`.
+pub trait DataFraction
+where
+    Self: DeserializeOwned + Serialize + Clone + Sync + Send + 'static,
+{
+}
+
+impl<T> DataFraction for T where T: DeserializeOwned + Serialize + Clone + Sync + Send + 'static {}
+
+/// Immutable state of a data flow.
+pub trait State: DataFraction {
+    /// `UpdateEvent` - that sent from a server to a client
+    type Delta: DataFraction;
+
+    /// `ControlEvent` - that send from a client to a server
+    type Query: DataFraction;
+
+    /// Generic name with pack.
+    ///
+    /// Used in spans and for rendering.
+    fn class() -> &'static str {
+        std::any::type_name::<Self>()
+    }
+
+    /// Applies the delta.
+    fn apply(&mut self, delta: Self::Delta);
+
+    /// Detaches a state to send it to a client (listener)
+    fn divide(&self) -> Self;
+
+    /// Packs the state.
+    fn pack_state(&self) -> Result<PackedState> {
+        FlexCodec::encode(self)
+    }
+
+    /// Unpacks the state.
+    fn unpack_state(data: &PackedState) -> Result<Self> {
+        FlexCodec::decode(data)
+    }
+
+    /// Packs the delta.
+    fn pack_delta(delta: &Self::Delta) -> Result<PackedEvent> {
+        FlexCodec::encode(delta)
+    }
+
+    /// Unpacks the delta.
+    fn unpack_delta(data: &PackedEvent) -> Result<Self::Delta> {
+        FlexCodec::decode(data)
+    }
+
+    /// Packs the action.
+    fn pack_query(query: &Self::Query) -> Result<PackedAction> {
+        FlexCodec::encode(query)
+    }
+
+    /// Unpacks the query.
+    fn unpack_query(data: &PackedAction) -> Result<Self::Query> {
+        FlexCodec::decode(data)
+    }
+}
